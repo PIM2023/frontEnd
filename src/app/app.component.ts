fe3e99e0cdb2ad1e8b44592dbf0ae960b69d193d
Marceldts <marceldeltoro23@gmail.com>
2023-10-31 17:53:45 +0100

    removed menu options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,6 @@ import { SignalsService } from './core/services/signals/signals.service';
 })
 export class AppComponent {
   public userSignal: WritableSignal<any> = signal({});
-  public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor(private signalsService: SignalsService) {
     this.signalsService.setUserSignal(this.userSignal);
   }
